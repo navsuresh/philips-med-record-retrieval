73e1d1cef4f6bb8cac2af47900520645b1f68c86
Naveen <naveen.213@gmail.com>
2019-09-12 10:08:22 +0530

    wmd and doc2vec chanegs

--- doc2vec.py
+++ doc2vec.py
@@ -15,16 +15,42 @@ import pandas as pd
 from sklearn import metrics
 import gensim.models as g
 import codecs
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+import copy
 
-path = '../clicr-master/dataset/no-ent/cnnlike/test/*'
+
+stopwords_1 = stopwords.words('english')
+print(type(stopwords_1))    
+stopwords_1 = set(stopwords_1)
+def preprocess(doc):
+    doc = doc.lower()  # Lower the text.
+    doc = word_tokenize(doc)  # Split into words.
+    
+    global stopwords_1
+#    stopwords_1 = set(stopwords_1)
+    doc = [w for w in doc if not w in stopwords_1]  # Remove stopwords.
+    doc = [w for w in doc if w.isalpha()]  # Remove numbers and punctuation.
+    return doc
+path = '../Philips/clicr/clicr/no-ent/cnnlike/test/*'
 docs = glob.glob(path)
-doc_rows = []
+doc_rows_og = []
 for doc in docs:
     f = open(doc, 'r')
-    doc_rows.append([f.read()])
+    doc_rows_og.append([f.read()])
     f.close()
 	
-doc_rows = [[i[0].split('\n')[2]] for i in doc_rows]    
+doc_rows_og = [[i[0].split('\n')[2]] for i in doc_rows_og]   
+ 
+doc_rows = copy.deepcopy(doc_rows_og)
+
+for i in range(len(doc_rows)):
+    doc_rows[i][0] = preprocess(doc_rows[i][0])
+    doc_rows[i][0] = ' '.join(doc_rows[i][0])
+
+    
+    
+
 train = pd.DataFrame(doc_rows, columns = (['text']))
 
 LabeledSentence1 = gensim.models.doc2vec.TaggedDocument
@@ -53,63 +79,66 @@ fname = "my_doc2vec.model"
 d2v_model.save(fname)
 
 s = ["severe myositis on commencement of efavirenz , abacavir and lamivudine , in the absence of lactic acidosis or classical abacavir hypersensitivity summary myositis in hiv may be due to hiv itself , or to opportunistic infection , malignancy or drug treatment . severe myositis or rhabdomyolysis have never been reported with the commonly used nucleoside reverse transcriptase inhibitor abacavir , although creatine phosphokinase may rise modestly , particularly if abacavir hypersensitivity occurs"]
+s[0] = preprocess(s[0])
+s[0] = ' '.join(s[0])
+print(s[0])
 d2v_model = Doc2Vec.load(fname)
 infer_vector = d2v_model.infer_vector(s)
-similar_documents = d2v_model.docvecs.most_similar([infer_vector], topn = 1)
+similar_documents = d2v_model.docvecs.most_similar([infer_vector], topn = 10)
 print('Document Id = ' + str(similar_documents[0][0]))
-
-# you can continue training with the loaded model!
-#If you’re finished training a model (=no more updates, only querying, reduce memory usage), you can do: 
-#d2v_model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
-
-
-kmeans_model = KMeans(n_clusters=4, init='k-means++', max_iter=100)
-X = kmeans_model.fit(d2v_model.docvecs.doctag_syn0)
-labels=kmeans_model.labels_.tolist()
-
-l = kmeans_model.fit_predict(d2v_model.docvecs.doctag_syn0)
-pca = PCA(n_components=2).fit(d2v_model.docvecs.doctag_syn0)
-datapoint = pca.transform(d2v_model.docvecs.doctag_syn0)
-
-plt.figure
-label1 = ["#FFFF00", "#008000", "#0000FF", "#800080"]
-color = [label1[i] for i in labels]
-plt.scatter(datapoint[:, 0], datapoint[:, 1], c=color)
-centroids = kmeans_model.cluster_centers_
-centroidpoint = pca.transform(centroids)
-plt.scatter(centroidpoint[:, 0], centroidpoint[:, 1], marker='^', s=150, c='#000000')
-plt.show()
- 
- 
-
-#inference hyper-parameters
-start_alpha=0.002
-infer_epoch=10
-
-test_docs="test_doc2.txt"
-#load model
-m = Doc2Vec.load(fname)
-test_docs = [x.strip().split() for x in codecs.open(test_docs, "r", "utf-8").readlines()]
- 
-X=[]
-for d in test_docs:     
-    X.append(m.infer_vector(d, alpha=start_alpha, steps=infer_epoch))
-
-k = 4 
- 
-from sklearn.cluster import Birch
- 
-brc = Birch(branching_factor=50, n_clusters=k, threshold=0.1, compute_labels=True)
-brc.fit(X)
- 
-clusters = brc.predict(X)
- 
-labels = brc.labels_
- 
-print("Clusters: ")
-print(clusters)
- 
-silhouette_score = metrics.silhouette_score(X, labels, metric='euclidean')
- 
-print ("Silhouette_score: ")
-print (silhouette_score)
+#
+## you can continue training with the loaded model!
+##If you’re finished training a model (=no more updates, only querying, reduce memory usage), you can do: 
+##d2v_model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
+#
+#
+#kmeans_model = KMeans(n_clusters=4, init='k-means++', max_iter=100)
+#X = kmeans_model.fit(d2v_model.docvecs.doctag_syn0)
+#labels=kmeans_model.labels_.tolist()
+#
+#l = kmeans_model.fit_predict(d2v_model.docvecs.doctag_syn0)
+#pca = PCA(n_components=2).fit(d2v_model.docvecs.doctag_syn0)
+#datapoint = pca.transform(d2v_model.docvecs.doctag_syn0)
+#
+#plt.figure
+#label1 = ["#FFFF00", "#008000", "#0000FF", "#800080"]
+#color = [label1[i] for i in labels]
+#plt.scatter(datapoint[:, 0], datapoint[:, 1], c=color)
+#centroids = kmeans_model.cluster_centers_
+#centroidpoint = pca.transform(centroids)
+#plt.scatter(centroidpoint[:, 0], centroidpoint[:, 1], marker='^', s=150, c='#000000')
+#plt.show()
+# 
+# 
+#
+##inference hyper-parameters
+#start_alpha=0.002
+#infer_epoch=10
+#
+#test_docs="test_doc2.txt"
+##load model
+#m = Doc2Vec.load(fname)
+#test_docs = [x.strip().split() for x in codecs.open(test_docs, "r", "utf-8").readlines()]
+# 
+#X=[]
+#for d in test_docs:     
+#    X.append(m.infer_vector(d, alpha=start_alpha, steps=infer_epoch))
+#
+#k = 4 
+# 
+#from sklearn.cluster import Birch
+# 
+#brc = Birch(branching_factor=50, n_clusters=k, threshold=0.1, compute_labels=True)
+#brc.fit(X)
+# 
+#clusters = brc.predict(X)
+# 
+#labels = brc.labels_
+# 
+#print("Clusters: ")
+#print(clusters)
+# 
+#silhouette_score = metrics.silhouette_score(X, labels, metric='euclidean')
+# 
+#print ("Silhouette_score: ")
+#print (silhouette_score)
--- wmd.py
+++ wmd.py
@@ -3,33 +3,49 @@ from gensim.models import KeyedVectors
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 import glob
+import copy
 model = KeyedVectors.load_word2vec_format('../pubmed2018_w2v_200D/pubmed2018_w2v_200D.bin', binary=True)
 
 #Normalize
 model.init_sims(replace=True)
 
-path = '../clicr-master/dataset/no-ent/cnnlike/test/*'
+path = '../Philips/clicr/clicr/no-ent/cnnlike/test/*'
 docs = glob.glob(path)
-doc_rows = []
+doc_rows_og = []
 for doc in docs:
     f = open(doc, 'r')
-    doc_rows.append([f.read()])
+    doc_rows_og.append([f.read()])
     f.close()
 	
-doc_rows = [[i[0].split('\n')[2]] for i in doc_rows]    
+doc_rows_og = [i[0].split('\n')[2] for i in doc_rows_og]
+ 
+doc_rows = copy.deepcopy(doc_rows_og)
+
+stopwords_1 = stopwords.words('english')
+stopwords_1 = set(stopwords_1)
+def preprocess(doc):
+    doc = doc.lower()  # Lower the text.
+    doc = word_tokenize(doc)  # Split into words.
+    
+    global stopwords_1
+#    stopwords_1 = set(stopwords_1)
+    doc = [w for w in doc if not w in stopwords_1]  # Remove stopwords.
+    doc = [w for w in doc if w.isalpha()]  # Remove numbers and punctuation.
+    return doc
+
+
+for i in range(len(doc_rows)):
+    doc_rows[i] = preprocess(doc_rows[i])
+#    doc_rows[i][0] = ' '.join(doc_rows[i][0])
+
+    
 
 
 
 from gensim.similarities import WmdSimilarity
 num_best = 10
-instance = WmdSimilarity(doc_rows, model, num_best=10)
 
-def preprocess(doc):
-    doc = doc.lower()  # Lower the text.
-    doc = word_tokenize(doc)  # Split into words.
-    doc = [w for w in doc if not w in stopwords]  # Remove stopwords.
-    doc = [w for w in doc if w.isalpha()]  # Remove numbers and punctuation.
-    return doc
+instance = WmdSimilarity(doc_rows, model, num_best=10)
 
 sent = 'Very good, you should seat outdoor.'
 query = preprocess(sent)
@@ -39,5 +55,4 @@ sims = instance[query]  # A query is simply a "look-up" in the similarity class.
 print('Query:')
 print(sent)
 for i in range(num_best):
-    print()
     print('sim = %.4f' % sims[i][1])
